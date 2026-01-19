62946813e306f89811c352881effb8b15bdd647e
erdi20 <rayadavid158@gmail.com>
2026-01-19 14:52:42 +0700

    Hapus Filament total, balik ke dashboard custom

--- resources/views/filament/components/weight-total.blade.php
+++
@@ -1,6 +0,0 @@
-<span class="{{ $color == 'success' ? 'text-green-600' : 'text-red-600' }} font-bold">
-    {{ $total }}%
-    @if ($total !== 100)
-        <span class="text-red-600"> (harus 100%)</span>
-    @endif
-</span>
--- resources/views/livewire/my-modern-profile/update-password-form.blade.php
+++
@@ -1,9 +0,0 @@
-<form wire:submit.prevent="updatePassword" class="space-y-6">
-    {{ $this->form }}
-
-    <div class="text-right">
-        <x-filament::button type="submit">
-            Ubah Kata Sandi
-        </x-filament::button>
-    </div>
-</form>
